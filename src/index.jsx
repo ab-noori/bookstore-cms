cb3ced51b152708c7a510f1a0879966ed8b02d33
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-10 20:38:55 +0330

    Update file extentions and remove boilerplates from codebase.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
