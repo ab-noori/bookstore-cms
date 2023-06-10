532ff430903506780d6a0ae55931fccb454a8ff0
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-10 20:32:57 +0330

    Add routing for pages.

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Categories from './pages/Categories';
+import Home from './pages/Home';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/bookstore-cms/" element={<Home />} />
+          <Route path="/bookstore-cms/categories" element={<Categories />} />
+        </Routes>
+      </main>
+      <footer>
+        <p className="copyright">&copy; 2023 Bookstor CMS | Developed by Abdulali Noori</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
