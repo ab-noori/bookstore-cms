f8c0ef1259b6b4c7279414d470411e96e2ae513d
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-14 22:12:47 +0330

    Add the categorires slicer.

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,17 @@
+// src/redux/categories/categoriesSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  status: 'Under construction',
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    // No additional reducers for categories slice
+  },
+});
+
+export default categoriesSlice.reducer;
