d0955eedf420a8ed94f40d81dd379bf9110c20e3
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-14 22:13:23 +0330

    Add the redux store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import categoriesReducer from './categories/categoriesSlice';
+import booksReducer from './books/booksSlice';
 
-export const store = configureStore({
-    reducer: {
-        
-    }
-})
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+
+export default store;
