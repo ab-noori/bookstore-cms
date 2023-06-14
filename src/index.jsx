88c9b45965a7997d504f36ea8704fd332fbaeb0d
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-14 22:14:23 +0330

    Include the redux provider.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
