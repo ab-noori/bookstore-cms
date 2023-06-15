cf3f527b041b77ffc2da69de294752ef0fa14dc5
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-15 11:11:31 +0330

    Remove linter errors.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,23 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   books: [
     {
-      "item_id": "item1",
-      "title": "The Great Gatsby",
-      "author": "John Smith",
-      "category": "Fiction"
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
     },
     {
-      "item_id": "item2",
-      "title": "Anna Karenina",
-      "author": "Leo Tolstoy",
-      "category": "Fiction"
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
     },
     {
-      "item_id": "item3",
-      "title": "The Selfish Gene",
-      "author": "Richard Dawkins",
-      "category": "Nonfiction"
-    }
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
   ],
 };
 
