8a87ca1f4da2743b6769c3e37a9853660fdad580
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-15 06:54:01 +0330

    Apply cleancode to codebase.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,4 +1,3 @@
-// src/redux/books/booksSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,4 +1,3 @@
-// src/redux/categories/categoriesSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
