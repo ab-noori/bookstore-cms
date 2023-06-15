2f874b21eb33aa3709506e34a51257c3ac8e340a
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-15 13:48:00 +0330

    Update the BookList component to handle remove button.

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import Book from './Book';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookList = ({ books, onDelete }) => (
-  <div>
-    <h2>Books</h2>
-    <ul>
-      {books.map((book) => (
-        <li key={book.id}>
-          <Book book={book} onDelete={onDelete} />
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const BookList = ({ books }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <div>
+      <h2>Books</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <Book book={book} onDelete={() => handleDelete(book.id)} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 BookList.propTypes = {
   books: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
     }),
   ).isRequired,
-  onDelete: PropTypes.func.isRequired,
 };
 
 export default BookList;
