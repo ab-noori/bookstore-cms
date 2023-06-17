dd2042a98e2a6445f6b6e562c73e7d0110f8806f
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-17 06:00:58 +0330

    Modify the bookSlice to add extra reducer for fetching data from api..

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,31 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {
-  books: [
-    {
-      id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-};
+const BOOKS_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/S61FxCfnjF5rOBxTB7gd/books';
+
+export const fetchBooks = createAsyncThunk('users/fetchBooks', async () => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+});
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState,
+  initialState: {
+    isLoading: false,
+    books: [],
+    error: null,
+  },
   reducers: {
     addBook: (state, action) => {
       const { title, author } = action.payload;
@@ -38,6 +31,26 @@ const booksSlice = createSlice({
     removeBook: (state, action) => {
       state.books = state.books.filter((book) => book.id !== action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.books = [];
+        Object.keys(action.payload).forEach((item) => {
+          const book = action.payload[item][0];
+          state.books.push({
+            id: item, title: book.title, author: book.author, category: book.category,
+          });
+        });
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
