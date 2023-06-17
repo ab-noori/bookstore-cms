66062fec0a7ea93ba509fb6f97cab0ac06d128ed
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-17 21:38:49 +0330

    Modify Navigation, Categories and index files for giving style.

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
